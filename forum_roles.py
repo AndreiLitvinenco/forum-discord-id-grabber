24d545736d75509cd042f5b3f0343bf7a92e68ff
Andrei <andreialitvinenco@gmail.com>
2024-05-11 22:22:56 +0300

    Cleanup: var

--- forum_roles.py
+++ forum_roles.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from var import variables as var
 import time
 from dotenv import load_dotenv
 import os
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from var import variables as var
 from dotenv import load_dotenv
 import os
 load_dotenv()
